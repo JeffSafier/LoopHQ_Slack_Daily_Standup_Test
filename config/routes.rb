0369a44360c21f37c2f41afec2cc0344dfc05e5c
Jeff Safier <jeff.safier@gmail.com>
2025-12-19 12:12:01 -0500

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  post "slack/interactions"
+  resources :slack , except: %i[index show create new update destroy edit ] do
+    collection do
+      post :interactions
+    end
+  end
+
+  # post "slack/interactions"
 
   # Defines the root path route ("/")
   # root "posts#index"
